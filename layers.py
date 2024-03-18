3b713aeea596a06eae06491882c32ddbcd70820b
Lucas Otávio Leme Silva <lucasotavio750@gmail.com>
2024-03-18 10:59:10 -0300

    Dense layer foward

---
+++ layers.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Dense():
+    def __init__(self, n_input, n_neurons):
+        self.weights = np.random.randn(n_input, n_neurons)
+        self.bias = np.random.randn(n_neurons)
+
+    def foward(self, input):
+        self.output = (input @ self.weights) + self.bias
+
+    def backward(self):
+        pass

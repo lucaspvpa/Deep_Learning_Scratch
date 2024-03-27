27e0ef7caa715fc4e1298a771f91f0a2e7f171ab
Lucas Otávio Leme Silva <lucasotavio750@gmail.com>
2024-03-27 15:12:12 -0300

    Adding Max value aux function

--- utils.py
+++ utils.py
@@ -9,5 +9,8 @@ def randn(shape, requires_grad=False):
     data = np.random.randn(*shape)
     return Tensor(data, requires_grad=requires_grad)
 
-def sum(a:Tensor, dim:int=-1, keepedims:bool=False):
-    return a.sum(dim=dim, keepdims=keepedims)
+def sum(tensor_a:Tensor, dim:int=-1, keepedims:bool=False):
+    return tensor_a.sum(dim=dim, keepdims=keepedims)
+
+def max(tensor_a: Tensor, dim: int=-1, keepdims: bool=False):
+    return tensor_a.max(dim=dim, keepdims=keepdims)

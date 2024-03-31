0a9b522129cdf8bbe03849c78a38c761ac6664f8
Lucas Otávio Leme Silva <lucasotavio750@gmail.com>
2024-03-31 20:54:49 -0300

    Momentum implemented

--- optimizer.py
+++ optimizer.py
@@ -1,11 +1,17 @@
+import numpy as np
+
 class SGD():
-    def __init__(self, parameters, lr):
+    def __init__(self, parameters, lr, momentum=0):
         self.parameters = parameters
         self.lr = lr
+        self.momentum = momentum
+        for parameter in self.parameters:
+            parameter.m = np.zeros(parameter.shape)
 
     def step(self):
         for parameter in self.parameters:
-            parameter._data = parameter._data - (self.lr * parameter.grad)
+            parameter.m = (self.momentum * parameter.m) - (self.lr * parameter.grad)
+            parameter._data = parameter._data + parameter.m
         
     def zero_grad(self):
         for parameter in self.parameters:

93ceb49bcf40349a3f1db2d6f03085220bd5c0f4
Lucas Otávio Leme Silva <lucasotavio750@gmail.com>
2024-03-31 21:37:50 -0300

    Nesterov and Weigh decay implemented

--- optimizer.py
+++ optimizer.py
@@ -1,17 +1,25 @@
 import numpy as np
 
 class SGD():
-    def __init__(self, parameters, lr, momentum=0):
+    def __init__(self, parameters, lr, momentum=0, nesterov=False, weigh_decay=0):
         self.parameters = parameters
         self.lr = lr
         self.momentum = momentum
+        self.nesterov = nesterov
+        self.weigh_decay = weigh_decay
         for parameter in self.parameters:
             parameter.m = np.zeros(parameter.shape)
 
     def step(self):
         for parameter in self.parameters:
-            parameter.m = (self.momentum * parameter.m) - (self.lr * parameter.grad)
-            parameter._data = parameter._data + parameter.m
+            gt = parameter.grad
+            if self.weigh_decay != 0:
+                gt = gt + (self.weigh_decay * parameter._data)
+            if self.momentum != 0:
+                parameter.m = (self.momentum * parameter.m) - gt
+                if self.nesterov:
+                    gt = gt + (self.momentum * parameter.m)
+            parameter._data = parameter._data - (self.lr * gt)
         
     def zero_grad(self):
         for parameter in self.parameters:

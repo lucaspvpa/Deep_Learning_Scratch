989325d385b3852963b73743c425208894f116bd
Lucas Otávio Leme Silva <lucasotavio750@gmail.com>
2024-04-01 13:37:07 -0300

    RMSProp implemented

--- optimizer.py
+++ optimizer.py
@@ -24,3 +24,26 @@ class SGD():
     def zero_grad(self):
         for parameter in self.parameters:
             parameter.zero_grad()
+
+
+class RMSProp:
+    def __init__(self, parameters, lr, decay_rate=0.9, eps=1e-8):
+        self.parameters = parameters
+        self.lr = lr
+        self.decay_rate = decay_rate
+        self.eps = eps
+        for parameter in self.parameters:
+            parameter.vt = np.zeros(parameter.shape)
+
+    def step(self):
+        for parameter in self.parameters:
+            gt = parameter.grad
+            parameter.vt = (self.decay_rate * parameter.vt) + (1 - self.decay_rate) * (gt**2)
+            parameter._data = parameter._data - (self.lr * gt / ((parameter.vt**0.5) + self.eps)) 
+
+    def zero_grad(self):
+        for parameter in self.parameters:
+            parameter.zero_grad()
+
+class Adam():
+    pass
